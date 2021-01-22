42f265f3978cb59ee7567e1e034bf758943056a7
Subhajit Sahu <wolfram77@gmail.com>
2021-01-22 23:19:00 +0530

    :bus: wrote tests

--- src/Boolean.elm
+++ src/Boolean.elm
@@ -42,13 +42,13 @@ Convert string to boolean.
     parse "not true" == False
     parse "inactive" == False
 -}
-parse: String -> Bool
+parse : String -> Bool
 parse s =
   let fal = contains (regex "(negati|never|refus|wrong|fal|off)|\\b(f|n|0)\\b") s
       neg = modBy 2 (length (find (regex "\\b(nay|nah|no|dis|un|in)") s)) == 1 in
   not (xor fal neg)
 
-regex: String -> Regex
+regex : String -> Regex
 regex s =
   withDefault Regex.never <| fromStringWith {multiline = False, caseInsensitive = True} s
 
@@ -64,7 +64,7 @@ Check if value is false.
     not False == True
     not True  == False
 -}
-not: Bool -> Bool
+not : Bool -> Bool
 not = Basics.not
 
 
@@ -79,7 +79,7 @@ Check if antecedent ⇔ consequent (a ⇔ b).
     eq True False  == False
     eq False True  == False
 -}
-eq: Bool -> Bool -> Bool
+eq : Bool -> Bool -> Bool
 eq = xnor2
 
 
@@ -94,9 +94,9 @@ Check if antecedent ⇒ consequent (a ⇒ b).
     imply False False == True
     imply True False  == False
 -}
-imply: Bool -> Bool -> Bool
+imply : Bool -> Bool -> Bool
 imply a b =
-  (not a) || b
+  not a || b
 
 
 {-|
@@ -110,8 +110,9 @@ Check if antecedent ⇏ consequent (a ⇏ b).
     nimply False True  == False
     nimply False False == False
 -}
-nimply: Bool -> Bool -> Bool
-nimply = not >> imply
+nimply : Bool -> Bool -> Bool
+nimply a b =
+  not <| imply a b
 
 
 
@@ -128,7 +129,7 @@ Check if antecedent ⇔ consequent (a ⇔ b).
     eqv True False  == False
     eqv False True  == False
 -}
-eqv: Bool -> Bool -> Bool
+eqv : Bool -> Bool -> Bool
 eqv = eq
 
 
@@ -143,7 +144,7 @@ Check if antecedent ⇒ consequent (a ⇒ b).
     imp False False == True
     imp True False  == False
 -}
-imp: Bool -> Bool -> Bool
+imp : Bool -> Bool -> Bool
 imp = imply
 
 
@@ -161,7 +162,7 @@ Check if all values are true.
     and False True  == False
     and False False == False
 -}
-and: Bool -> Bool -> Bool
+and : Bool -> Bool -> Bool
 and = and2
 
 
@@ -176,7 +177,7 @@ Check if all values are true.
     and False True  == False
     and False False == False
 -}
-and0: Bool
+and0 : Bool
 and0 =
   True
 
@@ -192,7 +193,7 @@ Check if all values are true.
     and False True  == False
     and False False == False
 -}
-and1: Bool -> Bool
+and1 : Bool -> Bool
 and1 a =
   a
 
@@ -208,7 +209,7 @@ Check if all values are true.
     and False True  == False
     and False False == False
 -}
-and2: Bool -> Bool -> Bool
+and2 : Bool -> Bool -> Bool
 and2 a b =
   a && b
 
@@ -224,7 +225,7 @@ Check if all values are true.
     and False True  == False
     and False False == False
 -}
-and3: Bool -> Bool -> Bool -> Bool
+and3 : Bool -> Bool -> Bool -> Bool
 and3 a b c =
   a && b && c
 
@@ -240,7 +241,7 @@ Check if all values are true.
     and False True  == False
     and False False == False
 -}
-and4: Bool -> Bool -> Bool -> Bool -> Bool
+and4 : Bool -> Bool -> Bool -> Bool -> Bool
 and4 a b c d =
   a && b && c && d
 
@@ -256,7 +257,7 @@ Check if all values are true.
     and False True  == False
     and False False == False
 -}
-and5: Bool -> Bool -> Bool -> Bool -> Bool -> Bool
+and5 : Bool -> Bool -> Bool -> Bool -> Bool -> Bool
 and5 a b c d e =
   a && b && c && d && e
 
@@ -272,7 +273,7 @@ Check if all values are true.
     and False True  == False
     and False False == False
 -}
-and6: Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool
+and6 : Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool
 and6 a b c d e f =
   a && b && c && d && e && f
 
@@ -288,7 +289,7 @@ Check if all values are true.
     and False True  == False
     and False False == False
 -}
-and7: Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool
+and7 : Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool
 and7 a b c d e f g =
   a && b && c && d && e && f && g
 
@@ -304,7 +305,7 @@ Check if all values are true.
     and False True  == False
     and False False == False
 -}
-and8: Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool
+and8 : Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool
 and8 a b c d e f g h =
   a && b && c && d && e && f && g && h
 
@@ -323,7 +324,7 @@ Check if any value is true.
     or False True  == True
     or False False == False
 -}
-or: Bool -> Bool -> Bool
+or : Bool -> Bool -> Bool
 or = and2
 
 
@@ -338,7 +339,7 @@ Check if any value is true.
     or False True  == True
     or False False == False
 -}
-or0: Bool
+or0 : Bool
 or0 =
   False
 
@@ -354,7 +355,7 @@ Check if any value is true.
     or False True  == True
     or False False == False
 -}
-or1: Bool -> Bool
+or1 : Bool -> Bool
 or1 a =
   a
 
@@ -370,7 +371,7 @@ Check if any value is true.
     or False True  == True
     or False False == False
 -}
-or2: Bool -> Bool -> Bool
+or2 : Bool -> Bool -> Bool
 or2 a b =
   a || b
 
@@ -386,7 +387,7 @@ Check if any value is true.
     or False True  == True
     or False False == False
 -}
-or3: Bool -> Bool -> Bool -> Bool
+or3 : Bool -> Bool -> Bool -> Bool
 or3 a b c =
   a || b || c
 
@@ -402,7 +403,7 @@ Check if any value is true.
     or False True  == True
     or False False == False
 -}
-or4: Bool -> Bool -> Bool -> Bool -> Bool
+or4 : Bool -> Bool -> Bool -> Bool -> Bool
 or4 a b c d =
   a || b || c || d
 
@@ -418,7 +419,7 @@ Check if any value is true.
     or False True  == True
     or False False == False
 -}
-or5: Bool -> Bool -> Bool -> Bool -> Bool -> Bool
+or5 : Bool -> Bool -> Bool -> Bool -> Bool -> Bool
 or5 a b c d e =
   a || b || c || d || e
 
@@ -434,7 +435,7 @@ Check if any value is true.
     or False True  == True
     or False False == False
 -}
-or6: Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool
+or6 : Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool
 or6 a b c d e f =
   a || b || c || d || e || f
 
@@ -450,7 +451,7 @@ Check if any value is true.
     or False True  == True
     or False False == False
 -}
-or7: Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool
+or7 : Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool
 or7 a b c d e f g =
   a || b || c || d || e || f || g
 
@@ -466,7 +467,7 @@ Check if any value is true.
     or False True  == True
     or False False == False
 -}
-or8: Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool
+or8 : Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool
 or8 a b c d e f g h =
   a || b || c || d || e || f || g || h
 
@@ -485,9 +486,8 @@ Check if odd no. of values are true.
     xor True True   == False
     xor False False == False
 -}
-xor: Bool -> Bool -> Bool
-xor =
-  xor2
+xor : Bool -> Bool -> Bool
+xor = xor2
 
 
 {-|
@@ -501,7 +501,7 @@ Check if odd no. of values are true.
     xor True True   == False
     xor False False == False
 -}
-xor0: Bool
+xor0 : Bool
 xor0 =
   False
 
@@ -517,7 +517,7 @@ Check if odd no. of values are true.
     xor True True   == False
     xor False False == False
 -}
-xor1: Bool -> Bool
+xor1 : Bool -> Bool
 xor1 a =
   a
 
@@ -533,7 +533,7 @@ Check if odd no. of values are true.
     xor True True   == False
     xor False False == False
 -}
-xor2: Bool -> Bool -> Bool
+xor2 : Bool -> Bool -> Bool
 xor2 = Basics.xor
 
 
@@ -548,7 +548,7 @@ Check if odd no. of values are true.
     xor True True   == False
     xor False False == False
 -}
-xor3: Bool -> Bool -> Bool -> Bool
+xor3 : Bool -> Bool -> Bool -> Bool
 xor3 a b c =
   xor2 (xor2 a b) (xor1 c)
 
@@ -564,7 +564,7 @@ Check if odd no. of values are true.
     xor True True   == False
     xor False False == False
 -}
-xor4: Bool -> Bool -> Bool -> Bool -> Bool
+xor4 : Bool -> Bool -> Bool -> Bool -> Bool
 xor4 a b c d =
   xor2 (xor2 a b) (xor2 c d)
 
@@ -580,7 +580,7 @@ Check if odd no. of values are true.
     xor True True   == False
     xor False False == False
 -}
-xor5: Bool -> Bool -> Bool -> Bool -> Bool -> Bool
+xor5 : Bool -> Bool -> Bool -> Bool -> Bool -> Bool
 xor5 a b c d e =
   xor2 (xor4 a b c d) (xor1 e)
 
@@ -596,7 +596,7 @@ Check if odd no. of values are true.
     xor True True   == False
     xor False False == False
 -}
-xor6: Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool
+xor6 : Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool
 xor6 a b c d e f =
   xor2 (xor4 a b c d) (xor2 e f)
 
@@ -612,7 +612,7 @@ Check if odd no. of values are true.
     xor True True   == False
     xor False False == False
 -}
-xor7: Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool
+xor7 : Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool
 xor7 a b c d e f g =
   xor2 (xor4 a b c d) (xor3 e f g)
 
@@ -628,7 +628,7 @@ Check if odd no. of values are true.
     xor True True   == False
     xor False False == False
 -}
-xor8: Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool
+xor8 : Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool
 xor8 a b c d e f g h =
   xor2 (xor4 a b c d) (xor4 e f g h)
 
@@ -647,7 +647,7 @@ Count no. of true values.
     count False True  == 1
     count False False == 0
 -}
-count: Bool -> Bool -> Int
+count : Bool -> Bool -> Int
 count = count2
 
 
@@ -662,8 +662,9 @@ Count no. of true values.
     count False True  == 1
     count False False == 0
 -}
-count0: Int
-count0 = 0
+count0 : Int
+count0 =
+  0
 
 
 {-|
@@ -677,7 +678,7 @@ Count no. of true values.
     count False True  == 1
     count False False == 0
 -}
-count1: Bool -> Int
+count1 : Bool -> Int
 count1 a =
   if a then 1 else 0
 
@@ -693,7 +694,7 @@ Count no. of true values.
     count False True  == 1
     count False False == 0
 -}
-count2: Bool -> Bool -> Int
+count2 : Bool -> Bool -> Int
 count2 a b =
   count1 a + count1 b
 
@@ -709,7 +710,7 @@ Count no. of true values.
     count False True  == 1
     count False False == 0
 -}
-count3: Bool -> Bool -> Bool -> Int
+count3 : Bool -> Bool -> Bool -> Int
 count3 a b c =
   count2 a b + count1 c
 
@@ -725,7 +726,7 @@ Count no. of true values.
     count False True  == 1
     count False False == 0
 -}
-count4: Bool -> Bool -> Bool -> Bool -> Int
+count4 : Bool -> Bool -> Bool -> Bool -> Int
 count4 a b c d =
   count2 a b + count2 c d
 
@@ -741,7 +742,7 @@ Count no. of true values.
     count False True  == 1
     count False False == 0
 -}
-count5: Bool -> Bool -> Bool -> Bool -> Bool -> Int
+count5 : Bool -> Bool -> Bool -> Bool -> Bool -> Int
 count5 a b c d e =
   count4 a b c d + count1 e
 
@@ -757,7 +758,7 @@ Count no. of true values.
     count False True  == 1
     count False False == 0
 -}
-count6: Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Int
+count6 : Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Int
 count6 a b c d e f =
   count4 a b c d + count2 e f
 
@@ -773,7 +774,7 @@ Count no. of true values.
     count False True  == 1
     count False False == 0
 -}
-count7: Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Int
+count7 : Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Int
 count7 a b c d e f g =
   count4 a b c d + count3 e f g
 
@@ -789,7 +790,7 @@ Count no. of true values.
     count False True  == 1
     count False False == 0
 -}
-count8: Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Int
+count8 : Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Int
 count8 a b c d e f g h =
   count4 a b c d + count4 e f g h
 
@@ -808,7 +809,7 @@ Check if any value is false.
     nand False False == True
     nand True True   == False
 -}
-nand: Bool -> Bool -> Bool
+nand : Bool -> Bool -> Bool
 nand = nand2
 
 
@@ -823,8 +824,9 @@ Check if any value is false.
     nand False False == True
     nand True True   == False
 -}
-nand0: Bool
-nand0 = not and0
+nand0 : Bool
+nand0 =
+  not <| and0
 
 
 {-|
@@ -838,8 +840,9 @@ Check if any value is false.
     nand False False == True
     nand True True   == False
 -}
-nand1: Bool -> Bool
-nand1 = not >> and1
+nand1 : Bool -> Bool
+nand1 a =
+  not <| and1 a
 
 
 {-|
@@ -853,8 +856,9 @@ Check if any value is false.
     nand False False == True
     nand True True   == False
 -}
-nand2: Bool -> Bool -> Bool
-nand2 = not >> and2
+nand2 : Bool -> Bool -> Bool
+nand2 a b =
+  not <| and2 a b
 
 
 {-|
@@ -868,8 +872,9 @@ Check if any value is false.
     nand False False == True
     nand True True   == False
 -}
-nand3: Bool -> Bool -> Bool -> Bool
-nand3 = not >> and3
+nand3 : Bool -> Bool -> Bool -> Bool
+nand3 a b c =
+  not <| and3 a b c
 
 
 {-|
@@ -883,8 +888,9 @@ Check if any value is false.
     nand False False == True
     nand True True   == False
 -}
-nand4: Bool -> Bool -> Bool -> Bool -> Bool
-nand4 = not >> and4
+nand4 : Bool -> Bool -> Bool -> Bool -> Bool
+nand4 a b c d =
+  not <| and4 a b c d
 
 
 {-|
@@ -898,8 +904,9 @@ Check if any value is false.
     nand False False == True
     nand True True   == False
 -}
-nand5: Bool -> Bool -> Bool -> Bool -> Bool -> Bool
-nand5 = not >> and5
+nand5 : Bool -> Bool -> Bool -> Bool -> Bool -> Bool
+nand5 a b c d e =
+  not <| and5 a b c d e
 
 
 {-|
@@ -913,8 +920,9 @@ Check if any value is false.
     nand False False == True
     nand True True   == False
 -}
-nand6: Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool
-nand6 = not >> and6
+nand6 : Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool
+nand6 a b c d e f =
+  not <| and6 a b c d e f
 
 
 {-|
@@ -928,8 +936,9 @@ Check if any value is false.
     nand False False == True
     nand True True   == False
 -}
-nand7: Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool
-nand7 = not >> and7
+nand7 : Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool
+nand7 a b c d e f g =
+  not <| and7 a b c d e f g
 
 
 {-|
@@ -943,8 +952,9 @@ Check if any value is false.
     nand False False == True
     nand True True   == False
 -}
-nand8: Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool
-nand8 = not >> and8
+nand8 : Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool
+nand8 a b c d e f g h =
+  not <| and8 a b c d e f g h
 
 
 
@@ -961,7 +971,7 @@ Check if all values are false.
     nor True False  == False
     nor False True  == False
 -}
-nor: Bool -> Bool -> Bool
+nor : Bool -> Bool -> Bool
 nor = nor2
 
 
@@ -976,8 +986,9 @@ Check if all values are false.
     nor True False  == False
     nor False True  == False
 -}
-nor0: Bool
-nor0 = not or0
+nor0 : Bool
+nor0 =
+  not or0
 
 
 {-|
@@ -991,8 +1002,9 @@ Check if all values are false.
     nor True False  == False
     nor False True  == False
 -}
-nor1: Bool -> Bool
-nor1 = not >> or1
+nor1 : Bool -> Bool
+nor1 a =
+  not <| or1 a
 
 
 {-|
@@ -1006,8 +1018,9 @@ Check if all values are false.
     nor True False  == False
     nor False True  == False
 -}
-nor2: Bool -> Bool -> Bool
-nor2 = not >> or2
+nor2 : Bool -> Bool -> Bool
+nor2 a b =
+  not <| or2 a b
 
 
 {-|
@@ -1021,8 +1034,9 @@ Check if all values are false.
     nor True False  == False
     nor False True  == False
 -}
-nor3: Bool -> Bool -> Bool -> Bool
-nor3 = not >> or3
+nor3 : Bool -> Bool -> Bool -> Bool
+nor3 a b c =
+  not <| or3 a b c
 
 
 {-|
@@ -1036,8 +1050,9 @@ Check if all values are false.
     nor True False  == False
     nor False True  == False
 -}
-nor4: Bool -> Bool -> Bool -> Bool -> Bool
-nor4 = not >> or4
+nor4 : Bool -> Bool -> Bool -> Bool -> Bool
+nor4 a b c d =
+  not <| or4 a b c d
 
 
 {-|
@@ -1051,8 +1066,9 @@ Check if all values are false.
     nor True False  == False
     nor False True  == False
 -}
-nor5: Bool -> Bool -> Bool -> Bool -> Bool -> Bool
-nor5 = not >> or5
+nor5 : Bool -> Bool -> Bool -> Bool -> Bool -> Bool
+nor5 a b c d e =
+  not <| or5 a b c d e
 
 
 {-|
@@ -1066,8 +1082,9 @@ Check if all values are false.
     nor True False  == False
     nor False True  == False
 -}
-nor6: Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool
-nor6 = not >> or6
+nor6 : Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool
+nor6 a b c d e f =
+  not <| or6 a b c d e f
 
 
 {-|
@@ -1081,8 +1098,9 @@ Check if all values are false.
     nor True False  == False
     nor False True  == False
 -}
-nor7: Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool
-nor7 = not >> or7
+nor7 : Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool
+nor7 a b c d e f g =
+  not <| or7 a b c d e f g
 
 
 {-|
@@ -1096,8 +1114,9 @@ Check if all values are false.
     nor True False  == False
     nor False True  == False
 -}
-nor8: Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool
-nor8 = not >> or8
+nor8 : Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool
+nor8 a b c d e f g h =
+  not <| or8 a b c d e f g h
 
 
 
@@ -1114,7 +1133,7 @@ Check if even no. of values are true.
     xnor True False  == False
     xnor False True  == False
 -}
-xnor: Bool -> Bool -> Bool
+xnor : Bool -> Bool -> Bool
 xnor = xnor2
 
 
@@ -1129,8 +1148,9 @@ Check if even no. of values are true.
     xnor True False  == False
     xnor False True  == False
 -}
-xnor0: Bool
-xnor0 = not xor0
+xnor0 : Bool
+xnor0 =
+  not xor0
 
 
 {-|
@@ -1144,8 +1164,9 @@ Check if even no. of values are true.
     xnor True False  == False
     xnor False True  == False
 -}
-xnor1: Bool -> Bool
-xnor1 = not >> xor1
+xnor1 : Bool -> Bool
+xnor1 a =
+  not <| xor1 a
 
 
 {-|
@@ -1159,8 +1180,9 @@ Check if even no. of values are true.
     xnor True False  == False
     xnor False True  == False
 -}
-xnor2: Bool -> Bool -> Bool
-xnor2 = not >> xor2
+xnor2 : Bool -> Bool -> Bool
+xnor2 a b =
+  not <| xor2 a b
 
 
 {-|
@@ -1174,8 +1196,9 @@ Check if even no. of values are true.
     xnor True False  == False
     xnor False True  == False
 -}
-xnor3: Bool -> Bool -> Bool -> Bool
-xnor3 = not >> xor3
+xnor3 : Bool -> Bool -> Bool -> Bool
+xnor3 a b c =
+  not <| xor3 a b c
 
 
 {-|
@@ -1189,8 +1212,9 @@ Check if even no. of values are true.
     xnor True False  == False
     xnor False True  == False
 -}
-xnor4: Bool -> Bool -> Bool -> Bool -> Bool
-xnor4 = not >> xor4
+xnor4 : Bool -> Bool -> Bool -> Bool -> Bool
+xnor4 a b c d =
+  not <| xor4 a b c d
 
 
 {-|
@@ -1204,8 +1228,9 @@ Check if even no. of values are true.
     xnor True False  == False
     xnor False True  == False
 -}
-xnor5: Bool -> Bool -> Bool -> Bool -> Bool -> Bool
-xnor5 = not >> xor5
+xnor5 : Bool -> Bool -> Bool -> Bool -> Bool -> Bool
+xnor5 a b c d e =
+  not <| xor5 a b c d e
 
 
 {-|
@@ -1219,8 +1244,9 @@ Check if even no. of values are true.
     xnor True False  == False
     xnor False True  == False
 -}
-xnor6: Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool
-xnor6 = not >> xor6
+xnor6 : Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool
+xnor6 a b c d e f =
+  not <| xor6 a b c d e f
 
 
 {-|
@@ -1234,8 +1260,9 @@ Check if even no. of values are true.
     xnor True False  == False
     xnor False True  == False
 -}
-xnor7: Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool
-xnor7 = not >> xor7
+xnor7 : Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool
+xnor7 a b c d e f g =
+  not <| xor7 a b c d e f g
 
 
 {-|
@@ -1249,5 +1276,6 @@ Check if even no. of values are true.
     xnor True False  == False
     xnor False True  == False
 -}
-xnor8: Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool
-xnor8 = not >> xor8
+xnor8 : Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool
+xnor8 a b c d e f g h =
+  not <| xor8 a b c d e f g h
--- tests/Example.elm
+++ tests/Example.elm
@@ -7,56 +7,292 @@ import Boolean exposing (..)
 
 
 
-xorTests : Test
-xorTests =
-  describe "xor x y" [
-    test "xor True True == False" <|
-      \_ -> Boolean.xor True True |> equal False,
-    test "xor True False == True" <|
-      \_ -> Boolean.xor True False |> equal True
+
+-- PARSE
+parseTests : Test
+parseTests =
+  describe "parse s" [
+    test "1.0" <|
+      \_ -> parse "not true" |> equal False,
+    test "1.1" <|
+      \_ -> parse "inactive" |> equal False,
+    test "1.2" <|
+      \_ -> parse "not off" |> equal True,
+    test "1.3" <|
+      \_ -> parse "truthy" |> equal True
   ]
 
 
 
+
+-- NOT, EQ, IMPLY, NIMPLY (FIXED)
+notTests : Test
+notTests =
+  describe "not a" [
+    test "1.0" <|
+      \_ -> Boolean.not False |> equal True,
+    test "1.1" <|
+      \_ -> Boolean.not True |> equal False
+  ]
+
+
+eqTests : Test
+eqTests =
+  describe "eq a b" [
+    test "2.3" <|
+      \_ -> eq True True |> equal True,
+    test "2.0" <|
+      \_ -> eq False False |> equal True,
+    test "2.2" <|
+      \_ -> eq True False |> equal False,
+    test "2.1" <|
+      \_ -> eq False True |> equal False
+  ]
+
+
+implyTests : Test
+implyTests =
+  describe "imply a b" [
+    test "2.3" <|
+      \_ -> imply True True |> equal True,
+    test "2.1" <|
+      \_ -> imply False True |> equal True,
+    test "2.0" <|
+      \_ -> imply False False |> equal True,
+    test "2.2" <|
+      \_ -> imply True False |> equal False
+  ]
+
+
+nimplyTests : Test
+nimplyTests =
+  describe "nimply a b" [
+    test "2.2" <|
+      \_ -> nimply True False |> equal True,
+    test "2.3" <|
+      \_ -> nimply True True |> equal False,
+    test "2.1" <|
+      \_ -> nimply False True |> equal False,
+    test "2.0" <|
+      \_ -> nimply False False |> equal False
+  ]
+
+
+
+
+-- EQV, IMP (SHORTCUTS)
 eqvTests : Test
 eqvTests =
-  describe "eqv x y" [
-    test "eqv False False == True" <|
-      \_ -> eqv False False |> equal True,
-    test "eqv True True == True" <|
+  describe "eqv a b" [
+    test "2.3" <|
       \_ -> eqv True True |> equal True,
-    test "eqv False True == False" <|
-      \_ -> eqv False True |> equal False,
-    test "eqv True False == False" <|
-      \_ -> eqv True False |> equal False
+    test "2.0" <|
+      \_ -> eqv False False |> equal True,
+    test "2.2" <|
+      \_ -> eqv True False |> equal False,
+    test "2.1" <|
+      \_ -> eqv False True |> equal False
   ]
-
 
 
 impTests : Test
 impTests =
-  describe "imp x y" [
-    test "imp False False == True" <|
-      \_ -> imp False False |> equal True,
-    test "imp False True == True" <|
-      \_ -> imp False True |> equal True,
-    test "imp True True == True" <|
+  describe "imp a b" [
+    test "2.3" <|
       \_ -> imp True True |> equal True,
-    test "imp True False == False" <|
+    test "2.1" <|
+      \_ -> imp False True |> equal True,
+    test "2.0" <|
+      \_ -> imp False False |> equal True,
+    test "2.2" <|
       \_ -> imp True False |> equal False
   ]
 
 
 
-parseTests : Test
-parseTests =
-  describe "parse s" [
-    test "parse \"not true\" == False" <|
-      \_ -> parse "not true" |> equal False,
-    test "parse \"inactive\" == False" <|
-      \_ -> parse "inactive" |> equal False,
-    test "parse \"not off\" == True" <|
-      \_ -> parse "not off" |> equal True,
-    test "parse \"truthy\" == True" <|
-      \_ -> parse "truthy" |> equal True
+
+-- AND (VARIABLE)
+andTests : Test
+andTests =
+  describe "and[n] a b ..." [
+    test "0.0" <|
+      \_ -> and0 |> equal True,
+    test "1.1" <|
+      \_ -> and1 True |> equal True,
+    test "1.0" <|
+      \_ -> and1 False |> equal False,
+    test "2.3" <|
+      \_ -> and2 True True |> equal True,
+    test "2.2" <|
+      \_ -> and2 True False |> equal False,
+    test "2.1" <|
+      \_ -> and2 False True |> equal False,
+    test "2.0" <|
+      \_ -> and2 False False |> equal False,
+    test "8.255" <|
+      \_ -> and8 True True True True True True True True |> equal True,
+    test "8.254" <|
+      \_ -> and8 True True True True True True True False |> equal False
+  ]
+
+
+
+
+-- OR (VARIABLE)
+orTests : Test
+orTests =
+  describe "or[n] a b ..." [
+    test "0.0" <|
+      \_ -> or0 |> equal False,
+    test "1.1" <|
+      \_ -> or1 True |> equal True,
+    test "1.0" <|
+      \_ -> or1 False |> equal False,
+    test "2.3" <|
+      \_ -> or2 True True |> equal True,
+    test "2.2" <|
+      \_ -> or2 True False |> equal True,
+    test "2.1" <|
+      \_ -> or2 False True |> equal True,
+    test "2.0" <|
+      \_ -> or2 False False |> equal False,
+    test "8.1" <|
+      \_ -> or8 False False False False False False False True |> equal True,
+    test "8.0" <|
+      \_ -> or8 False False False False False False False False |> equal False
+  ]
+
+
+
+
+-- XOR (VARIABLE)
+xorTests : Test
+xorTests =
+  describe "xor[n] a b ..." [
+    test "0.0" <|
+      \_ -> xor0 |> equal False,
+    test "1.1" <|
+      \_ -> xor1 True |> equal True,
+    test "1.0" <|
+      \_ -> xor1 False |> equal False,
+    test "2.2" <|
+      \_ -> xor2 True False |> equal True,
+    test "2.1" <|
+      \_ -> xor2 False True |> equal True,
+    test "2.3" <|
+      \_ -> xor2 True True |> equal False,
+    test "2.0" <|
+      \_ -> xor2 False False |> equal False,
+    test "8.1" <|
+      \_ -> xor8 False False False False False False False True |> equal True,
+    test "8.3" <|
+      \_ -> xor8 False False False False False False True True |> equal False
+  ]
+
+
+
+
+-- COUNT (VARIABLE)
+countTests : Test
+countTests =
+  describe "count[n] a b ..." [
+    test "0.0" <|
+      \_ -> count0 |> equal 0,
+    test "1.1" <|
+      \_ -> count1 True |> equal 1,
+    test "1.0" <|
+      \_ -> count1 False |> equal 0,
+    test "2.3" <|
+      \_ -> count2 True True |> equal 2,
+    test "2.2" <|
+      \_ -> count2 True False |> equal 1,
+    test "2.1" <|
+      \_ -> count2 False True |> equal 1,
+    test "2.0" <|
+      \_ -> count2 False False |> equal 0,
+    test "8.1" <|
+      \_ -> count8 False False False False False False False True |> equal 1,
+    test "8.7" <|
+      \_ -> count8 False False False False False True True True |> equal 3
+  ]
+
+
+
+
+-- NAND (VARIABLE)
+nandTests : Test
+nandTests =
+  describe "nand[n] a b ..." [
+    test "0.0" <|
+      \_ -> nand0 |> equal False,
+    test "1.1" <|
+      \_ -> nand1 False |> equal True,
+    test "1.0" <|
+      \_ -> nand1 True |> equal False,
+    test "2.2" <|
+      \_ -> nand2 True False |> equal True,
+    test "2.1" <|
+      \_ -> nand2 False True |> equal True,
+    test "2.0" <|
+      \_ -> nand2 False False |> equal True,
+    test "2.3" <|
+      \_ -> nand2 True True |> equal False,
+    test "8.254" <|
+      \_ -> nand8 True True True True True True True False |> equal True,
+    test "8.255" <|
+      \_ -> nand8 True True True True True True True True |> equal False
+  ]
+
+
+
+
+-- NOR (VARIABLE)
+norTests : Test
+norTests =
+  describe "nor[n] a b ..." [
+    test "0.0" <|
+      \_ -> nor0 |> equal True,
+    test "1.1" <|
+      \_ -> nor1 False |> equal True,
+    test "1.0" <|
+      \_ -> nor1 True |> equal False,
+    test "2.0" <|
+      \_ -> nor2 False False |> equal True,
+    test "2.3" <|
+      \_ -> nor2 True True |> equal False,
+    test "2.2" <|
+      \_ -> nor2 True False |> equal False,
+    test "2.1" <|
+      \_ -> nor2 False True |> equal False,
+    test "8.0" <|
+      \_ -> nor8 False False False False False False False False |> equal True,
+    test "8.1" <|
+      \_ -> nor8 False False False False False False False True |> equal False
+  ]
+
+
+
+
+-- XNOR (VARIABLE)
+xnorTests : Test
+xnorTests =
+  describe "xnor[n] a b ..." [
+    test "0.0" <|
+      \_ -> xnor0 |> equal True,
+    test "1.1" <|
+      \_ -> xnor1 False |> equal True,
+    test "1.0" <|
+      \_ -> xnor1 True |> equal False,
+    test "2.3" <|
+      \_ -> xnor2 True True |> equal True,
+    test "2.0" <|
+      \_ -> xnor2 False False |> equal True,
+    test "2.2" <|
+      \_ -> xnor2 True False |> equal False,
+    test "2.1" <|
+      \_ -> xnor2 False True |> equal False,
+    test "8.0" <|
+      \_ -> xnor8 False False False False False False False False |> equal True,
+    test "8.7" <|
+      \_ -> xnor8 False False False False False True True True |> equal False
   ]
